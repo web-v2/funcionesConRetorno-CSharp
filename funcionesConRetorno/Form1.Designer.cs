fce799b9cf739259e52b9a87807c71bd6b2d6a3b
Samir Vergara V <svergara3@misena.edu.co>
2023-08-21 21:21:32 -0500

    Init Project

--- funcionesConRetorno/Form1.Designer.cs
+++ funcionesConRetorno/Form1.Designer.cs
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(51, 51);
+            label1.Name = "label1";
+            label1.Size = new Size(107, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Valor de la Compra";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(51, 69);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(199, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(53, 131);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Descuento";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(51, 149);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(199, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(54, 223);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(199, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(56, 205);
+            label3.Name = "label3";
+            label3.Size = new Size(61, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Valor Total";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.Red;
+            button1.Location = new Point(81, 286);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 44);
+            button1.TabIndex = 6;
+            button1.Text = "Calcular";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(307, 372);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Descuentos Store";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label3;
+        private Button button1;
     }
 }
--- funcionesConRetorno/Form1.cs
+++ funcionesConRetorno/Form1.cs
@@ -6,5 +6,37 @@ namespace funcionesConRetorno
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double descCompra = calcularDescuento(Double.Parse(textBox1.Text));
+
+            double compraFinal = Double.Parse(textBox1.Text) - descCompra;
+
+            textBox2.Text = descCompra.ToString();
+            textBox3.Text = compraFinal.ToString();
+        }
+
+        public double calcularDescuento(double totalC)
+        {
+            double desc = 0;
+
+            if (totalC > 100 && totalC <= 200)
+            {
+                desc = totalC*0.05;
+            }
+
+            if (totalC > 200 && totalC <= 300)
+            {
+                desc = totalC * 0.1;
+            }
+
+            if (totalC > 300)
+            {
+                desc = totalC * 0.2;
+            }
+
+            return desc;
+        }
     }
 }
